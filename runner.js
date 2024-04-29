2481f9ee718ac9c09b26c0c1fae4528473358646
Cheetos1131 <68630002+Cheetos1131@users.noreply.github.com>
2024-04-28 21:49:22 -0400

    test out winclose

--- runner.js
+++ runner.js
@@ -2,10 +2,23 @@ window.setInterval(timer, 60000);
 function timer() {
     alert("You have spent too much time here, goodbye.");
     alert("You Will Be Sent Somewhere Else.")
-    kickOut();
+    //kickOut();
     //Bye bye
+    winOpen();
 }
+let open1;
+let open2 = "https://Cheetos1131.github.io/index.html";
 
 function kickOut() {
-    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+    //window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+
+}
+
+function winOpen() {
+    open1 = window.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+    setTimeout(winClose, 12000);
+}
+
+function winClose() {
+    open1.close();
 }
