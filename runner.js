17fd75d901ea328db6185728646702477790a84b
Cheetos1131 <68630002+Cheetos1131@users.noreply.github.com>
2024-04-28 21:38:29 -0400

    test kick

--- runner.js
+++ runner.js
@@ -1,4 +1,11 @@
 window.setInterval(timer, 60000);
 function timer() {
-    alert("You have been on this page for 1 minute.");
+    alert("You have spent too much time here, goodbye.");
+    alert("You Will Be Sent Somewhere Else.")
+    kickOut();
+    //Bye bye
+}
+
+function kickOut() {
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"; 
 }
